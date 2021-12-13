84846a41f94261832168ca44bef050840e2b27ec
gurjeetsinghvirdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2021-12-14 00:42:12 +0530

    assign a value

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 function calculate() {
+
+    let bmi;
+    const result = document.getElementById("result");
+
     const weight = parseInt(document.getElementById("weight").value);
     document.getElementById("weight-value").textContent = weight + " kg";
 
     const height = parseInt(document.getElementById("height").value);
     document.getElementById("height-value").textContent = height + " cm";
+
+    bmi = (weight / Math.pow( (height/100), 2)).toFixed(1);
+    result.textContent = bmi;
+
+    if(bmi < 18.5) {
+        category = "UnderWeight"
+    }
+    else if(bmi >= 18.5 && bmi <= 24.9 ) {
+        category = "Normal Weight";
+    }
+    else if(bmi >= 25 && bmi <= 29.9) {
+        category = "Overweight"
+    }
+    else {
+        category = "Obese";
+    }
+    document.getElementById("category").textContent = category;
 }
