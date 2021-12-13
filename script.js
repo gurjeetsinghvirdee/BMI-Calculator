dbb066c9cfb28d08ec203b01fb1c802924cb071c
gurjeetsinghvirdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2021-12-14 00:50:39 +0530

    styled a category on color based

--- script.js
+++ script.js
@@ -14,15 +14,19 @@ function calculate() {
 
     if(bmi < 18.5) {
         category = "UnderWeight"
+        result.style.color = "#ffea00"
     }
     else if(bmi >= 18.5 && bmi <= 24.9 ) {
         category = "Normal Weight";
+        result.style.color = "#00ff00"
     }
     else if(bmi >= 25 && bmi <= 29.9) {
         category = "Overweight"
+        result.style.color = "#ffa500"
     }
     else {
         category = "Obese";
+        result.style.color = "#ff0000"
     }
     document.getElementById("category").textContent = category;
 }
